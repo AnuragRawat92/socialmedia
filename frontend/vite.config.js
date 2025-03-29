f9ef64f5fb24fc9eae878b55514f3248a59607af
AnuragRawat92 <148265902+AnuragRawat92@users.noreply.github.com>
2025-03-29 13:02:34 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
     "/api":{
-      target:"http://localhost:7000",
+      target:"https://socialmedia-s1pl.onrender.com",
       changeOrigin: true,
         secure: false,
     },
